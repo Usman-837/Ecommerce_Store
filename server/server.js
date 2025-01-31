8d272d856d803ec3be1094e8c37334d64c14ab5f
Usman <usmantufail837@gmail.com>
2025-01-31 10:47:42 +0500

    configure cors authentications, use cookieparser, express and port the server in server.js file

--- server/server.js
+++ server/server.js
@@ -14,5 +14,21 @@ const app = express()
 const PORT = process.env.PORT || 5000;
 
 app.use(
-    cors({})
+    cors({
+        origin : 'http://localhost:5173/',
+        methods : ['GET', 'POST', 'DELETE', 'PUT'],
+        allowedHeaders : [
+            'Content-Type',
+            'Authorization',
+            'Cache-Control',
+            'Expires',
+            'Pragma'
+        ],
+        credentials : true 
+    })
 )
+
+app.use(cookieParser());
+app.use(express.json());
+
+app.listen(PORT, ()=> console.log(`Server is now running on port ${PORT}`))
