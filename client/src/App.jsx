83a4337180d7f8247dfda5e4f72743fe75bc8440
Usman <usmantufail837@gmail.com>
2025-04-11 10:15:39 +0500

    create admin view layout and diffrent pages components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,17 +3,27 @@ import AuthLayout from './components/auth/layout'
 import AuthLogin from './Pages/auth/login'
 import AuthRegister from './Pages/auth/register'
 import './index.css'
+import AdminLayout from './components/admin-view/layout'
+import AdminDashboard from './Pages/admin-view/dashboard'
+import AdminFeatures from './Pages/admin-view/features'
+import AdminOrders from './Pages/admin-view/orders'
+import AdminProducts from './Pages/admin-view/products'
 
 function App() {
   return (
     <div className='flex flex-col overflow-hidden bg-white'>
       {/* common component */}
       <h1>Header component</h1>
-
       <Routes>
         <Route path='/auth' element={<AuthLayout />}>
           <Route path='login' element={<AuthLogin />} />
           <Route path='register' element={<AuthRegister />} />
+        </Route>
+        <Route path='/admin' element={<AdminLayout/>}> 
+          <Route path='dashboard' element={<AdminDashboard/>}/>
+          <Route path='products' element={<AdminProducts/>}/>
+          <Route path='orders' element={<AdminOrders/>}/>
+          <Route path='features' element={<AdminFeatures/>}/>
         </Route>
       </Routes>
     </div>
